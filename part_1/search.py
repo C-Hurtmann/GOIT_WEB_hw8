a5f98859936e7f49bcdcbf42a55c234a6c81eaf0
C-Hurtmann <constantine2903@gmail.com>
2023-05-20 20:21:35 +0300

    connect RedisLRU

--- part_1/search.py
+++ part_1/search.py
@@ -1,20 +1,28 @@
+import re
+from redis import StrictRedis
+from redis_lru import RedisLRU
+
 import connection
 from models import Quotes, Authors
-import difflib
-import re
 
+
+client = StrictRedis(host='localhost', port=6379, password=None)
+cache = RedisLRU(client)
+
+@cache()
 def get_mongo_keys():
     notes = []
     authors = set()
     for i in Quotes.objects:
         notes.extend(i.tags)
         authors.add(i.author.fullname)
-    return notes, authors
+    return [notes, list(authors)]
 
 def confirm_key(key, keys):
     pattern = re.compile(fr'{key}.*')
     return filter(lambda x: re.match(pattern, x), keys)
 
+@cache
 def execute_query(command: str, value: str):
     notes, authors = get_mongo_keys()
     if command == 'name':
@@ -26,7 +34,6 @@ def execute_query(command: str, value: str):
         value = value.split(',')
         value = list(map(lambda x: confirm_key(x, notes), value))
         value = [x for l in value for x in l]
-        print(value)
         result = Quotes.objects.filter(tags__in=value)
     return result
 
@@ -43,7 +50,7 @@ def main():
         result = execute_query(*query)
         for i in result:
             print(i)
-        print('-' * 20)
+        print('-' * 80)
         n += 1
 
 if __name__ =='__main__':
